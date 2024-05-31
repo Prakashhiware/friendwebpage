a6be7c77d14c03d5108095fd533e87e269643ecf
Prakashhiware <118271201+Prakashhiware@users.noreply.github.com>
2024-05-31 17:05:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const homeButton = document.getElementById('homeButton');
+    homeButton.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
